d8fe6c00fc75f09651943eaa398b77e6698b039e
PradeepKumar21-aws <dilleep844@gmail.com>
2026-06-01 16:58:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-#uvicorn main:app --reload
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
 templates = Jinja2Templates(directory="/code")
 
+# Homepage -> Pradeep Portfolio
 @app.get("/")
-def form_post(request: Request):
-    return templates.TemplateResponse('portfolio.html', context={'request': request})
+def home(request: Request):
+    return templates.TemplateResponse(
+        "new.html",
+        {"request": request}
+    )
 
-@app.get("/new")
-def form_post(request: Request):
-    return templates.TemplateResponse('new.html', context={'request': request})
+# Optional old page
+@app.get("/portfolio")
+def portfolio(request: Request):
+    return templates.TemplateResponse(
+        "portfolio.html",
+        {"request": request}
+    )
